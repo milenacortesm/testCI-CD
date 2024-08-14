4a0324d8a98a9a1a8576ca69a8171f2e6591ba65
milenacortesm <mile961121@gmail.com>
2024-08-14 14:08:43 -0300

    adding jenkins file to the project

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'q28mwh',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    experimentalWebKitSupport: true,
+    excludeSpecPattern: ['**/jsonObject.cy.js'] // Ignorar este archivo o carpeta
+  
   },
+
+
 });
